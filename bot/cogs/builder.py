d04a30b488bae7fa6e9ada601f982c4ad7f17452
aven7960-afk <aven7960@gmail.com>
2026-09-15 22:47:57 +0530

    Use fixed Components V2 modal compatibility layer

--- bot/cogs/builder.py
+++ bot/cogs/builder.py
@@ -5,7 +5,7 @@ from discord import app_commands
 from discord.ext import commands
 
 from bot.config import Settings
-from bot.views.builder_plus import BuilderPlusView
+from bot.views.builder_plus_fixed import BuilderPlusView
 
 
 class BuilderCog(commands.Cog):
